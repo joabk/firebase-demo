d4447d3d2aef0738ec8057ba2c2ae568036e8aff
joabk <mjoab88@gmail.com>
2020-01-31 16:57:08 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 //import { AngularFirestoreModule} from 'angularfire2/firestore';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
